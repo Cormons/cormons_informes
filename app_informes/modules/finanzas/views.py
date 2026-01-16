1ddf2b11a167ebbab5d50f499a2b1687499ceeb6
mfCormons <mf.soportecormons@gmail.com>
2026-01-16 10:30:38 -0300

    elimina un comentario de import

--- app_informes/modules/finanzas/views.py
+++ app_informes/modules/finanzas/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from app_informes.utils import obtener_datos_cookies
-from app_informes.core.vfp_comandos import formatear_fecha  # ← NUEVO IMPORT
+from app_informes.core.vfp_comandos import formatear_fecha
 from .vfp_comandos import comando_chequesCartera
 
 @require_http_methods(["GET"])
